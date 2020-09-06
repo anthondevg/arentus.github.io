d961db0c9cdf0bc9750814dae0cd3004658f3f34
Anthony Gonzalez <robwert7991@gmail.com>
2020-09-05 21:13:13 -0400

    contact form improved

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -17,50 +17,13 @@ var messagesRef = firebase.database().ref("messages");
 
 let typed = undefined;
 
-function resetTyped(newTexts) {
-  if (typed && typed.constructor === Typed) {
-    typed.destroy();
-  }
-
-  typed = new Typed("#input-contact", {
-    strings: newTexts,
-    typeSpeed: 0,
-    backSpeed: 0,
-    attr: "placeholder",
-    bindInputFocusEvents: true,
-    loop: true,
-    cursorChar: "",
-  });
-}
-
-var nameHolders = [
-  "John Doe",
-  "Peter Parker",
-  "Bruce Wayne",
-  "John Wick",
-  "Albert Einstein",
-];
-
-var emailHolders = [
-  "robwertm@gmail.com",
-  "anthot@mail.com",
-  "Bruce@mail.com",
-  "jwick@qmail.com",
-];
-
 // next button
-var next = document.getElementById("next-button");
-var send = document.getElementById("send-button");
-
-// label for input
-var labelInput = document.getElementById("label-input");
-// input values
-var inputContact = document.getElementById("input-contact");
-var inputMesssage = document.getElementById("message");
+var send = document.getElementById("send");
 
 // badges container
 var dataBadges = document.getElementById("dataForm");
 var successAlert = document.getElementById("successWrapper");
+var failureAlert = document.getElementById("failureWrapper");
 
 var contactData = {
   name: "",
@@ -68,83 +31,40 @@ var contactData = {
   message: "",
 };
 
-var nTL = {
-  0: "name",
-  1: "email",
-  2: "message",
-};
-
-var labelMessage = {
-  0: "What is your name?",
-  1: "What is your email?",
-  2: "Write your awesome message!",
-};
-
-var step = 0;
-resetTyped(nameHolders);
-
-next.addEventListener("click", function (e) {
-  e.preventDefault();
-
-  contactData[nTL[step]] = inputContact.value;
-
-  dataBadges.innerHTML += `<span class="badge badge-secondary  mr-2" id="name-badge">${inputContact.value}</span>`;
-  console.log(contactData);
-
-  step++;
-  // reinicia el typed y ponlo a mostrar emails
-  if (step == 1) {
-    resetTyped(emailHolders);
-  }
-  // mensaje sera escrito asi que oculta el input y reemplazalo por un textarea
-  if (step == 2) {
-    inputContact.style.display = "none";
-    inputMesssage.style.display = "block";
-    inputMesssage.focus();
-    inputMesssage.style.resize = "vertical";
-    inputMesssage.rows = 3;
-    next.disabled = true;
-
-    send.disabled = false;
-    send.classList.add("btn-primary");
-  }
-
-  // reinicia el valor
-  inputContact.value = "";
-  labelInput.innerHTML = labelMessage[step];
-});
-
 send.addEventListener("click", function (e) {
   e.preventDefault();
-  contactData[nTL[step]] = inputContact.value;
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+
+  console.log(name, email, message);
+
   // sending progress button
 
   send.innerText = "Sending...";
+
   // simulacion de peticion a api
+  successAlert.style.display = "none";
+  failureAlert.style.display = "none";
 
-  saveMessage(contactData.name, contactData.email, contactData.message);
+  if (name !== "" && email !== "" && message !== "") {
+    saveMessage(name, email, message);
 
-  var timer = setTimeout(function () {
-    // revert all to original state
-    send.innerText = "SEND";
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
 
-    send.classList.remove("btn-primary");
-
-    inputContact.value = "";
-    inputMesssage.value = "";
-    inputContact.style.display = "block";
-    inputMesssage.style.display = "none";
-
-    next.disabled = false;
-    send.disabled = true;
-    dataBadges.innerHTML = "";
-
-    contactData = {};
-    successAlert.style.display = "block";
-    step = 0;
-    labelInput.innerHTML = labelMessage[step];
-    resetTyped(nameHolders);
-  }, 2000);
+    setTimeout(function () {
+      // revert all to original state
+      successAlert.style.display = "block";
+      send.innerText = "Send Message";
+    }, 1000);
+  } else {
+    setTimeout(function () {
+      failureAlert.style.display = "block";
+      send.innerText = "Send Message";
+    }, 1000);
+  }
 });
 
 // Save message to firebase
@@ -156,20 +76,6 @@ function saveMessage(name, email, message) {
     message: message,
   });
 }
-
-// sincroniza el valor del textarea con el input original
-inputMesssage.addEventListener("keyup", function (e) {
-  inputContact.value = e.target.value;
-});
-
-// si la data es valida activa o desactiva el boton de next
-inputContact.addEventListener("keyup", function (e) {
-  if (validateData(inputContact.value, nTL[step])) {
-    next.disabled = false;
-  } else {
-    next.disabled = true;
-  }
-});
 
 function validateData(data, field) {
   switch (field) {
